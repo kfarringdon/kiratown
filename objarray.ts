bf37bdf6a7e8fa1b550628b597b34ff718dbc7c1
K <199493697+kfarringdon@users.noreply.github.com>
2025-06-01 18:41:49 +0100

    wordle

---
+++ objarray.ts
@@ -0,0 +1,16 @@
+const episodes = [
+  {
+    episode: 7,
+    title: "cool ep",
+  },
+  {
+    episode: 8,
+    title: "BAD ep",
+  },
+]
+
+console.log(episodes[3].title)
+
+const twodarray = ["hey", "bye", ["asdf", "sdf"]]
+
+console.log(twodarray[2][0])
